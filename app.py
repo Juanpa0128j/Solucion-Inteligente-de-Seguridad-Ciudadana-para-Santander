a8d0a84f1b86aae07f7641d1c5eb48b7d462b795
Veritoo123 <112337712+Veritoo123@users.noreply.github.com>
2025-11-28 17:16:41 -0500

    fix: chatbot app port

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ Es totalmente compatible con HTML enriquecido porque no se usa Markdown."""
 
 from flask import Flask, render_template, request
 from chatbot import responder
+import os
 
 app = Flask(__name__)
 
@@ -27,4 +28,7 @@ def index():
     return render_template("index.html", respuesta=respuesta)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
